d6d29b24937b2199c70dce7e12c089fb03b71e83
Kevin Trogant <greenfoxlight.42@gmail.com>
2016-12-05 09:41:04 +0100

    Setup django project

---
+++ PROJECT/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kevintrogant/Documents/3_semester/se/ue/Project_Pizza/PROJECT/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

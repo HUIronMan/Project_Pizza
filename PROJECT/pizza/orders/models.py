4c29393bad967cea472ca5b56e581e3df12c999a
Kevin Trogant <greenfoxlight.42@gmail.com>
2017-01-09 14:02:31 +0100

    Applied migrations to the database and implemented a sample test
    Tests should be added inside orders/tests.py

--- PROJECT/pizza/orders/models.py
+++ PROJECT/pizza/orders/models.py
@@ -1,23 +1,7 @@
+import datetime
+
 from django.db import models
-
-class Order(models.Model):
-    """Models an Order.
-    A order consists of a customer receiving the order, a number of pizzas and a set of extra toppings.
-    """
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    pizzas = models.ManyToManyField(Pizza)
-    toppings = models.ManyToManyField(Topping)
-
-class Customer(models.Model):
-    """A customer. Has a name and an address"""
-    name = models.CharField(max_length=200)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
-
-class Address(models.Model):
-    """A customer address."""
-    street = models.CharField(max_length=200) # Ex: Sample Street 42
-    postal_code = models.CharField(max_length=5) # exactly 5 digits
-    city = models.CharField(max_length=200)
+from django.utils import timezone
 
 class Pizza(models.Model):
     """A pizza"""
@@ -30,3 +14,33 @@ class Topping(models.Model):
     name = models.CharField(max_length=80)
     price = models.IntegerField(default=0) # In cents
     pass
+
+class Address(models.Model):
+    """A customer address."""
+    street = models.CharField(max_length=200) # Ex: Sample Street 42
+    postal_code = models.CharField(max_length=5) # exactly 5 digits
+    city = models.CharField(max_length=200)
+
+
+class Customer(models.Model):
+    """A customer. Has a name and an address"""
+    name = models.CharField(max_length=200)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    """Models an Order.
+    A order consists of a customer receiving the order, a number of pizzas and a set of extra toppings.
+    and the date at which the order was created.
+    """
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    pizzas = models.ManyToManyField(Pizza)
+    toppings = models.ManyToManyField(Topping)
+    received_date = models.DateTimeField('date received')
+    
+    def is_getting_old(self):
+        """Is this order older then 1 hour?"""
+        return not self.is_new()
+
+    def is_new(self):
+        """Is this order newer then 1 hour?"""
+        return self.received_date >= timezone.now() + datetime.timedelta(hours=1)
--- PROJECT/pizza/orders/tests.py
+++ PROJECT/pizza/orders/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+
+from .models import Order
+
+# Create your tests here
+class OrderMethodTests(TestCase):
+    def test_is_getting_old_with_future_order(self):
+        """
+        Is_getting_old() should return False for orders where received_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(hours=1)
+        future_order = Order(received_date=time)
+        self.assertIs(future_order.is_getting_old(), False)

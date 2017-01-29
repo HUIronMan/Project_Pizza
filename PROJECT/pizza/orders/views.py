cd2b6a309c56ee28cd94ece690ae892cc218fe1f
Kevin Trogant <greenfoxlight.42@gmail.com>
2017-01-29 17:55:38 +0100

    Fixed incompatability in bin/activate and moved functionality into utils.py

--- PROJECT/pizza/orders/views.py
+++ PROJECT/pizza/orders/views.py
@@ -5,40 +5,10 @@ from django.views import generic
 from django.utils import timezone
 
 from .models import MenuPizza, MenuTopping, Cart, OrderPizza, OrderTopping, Order, MenuSize, OrderSize
+from .util import *
 
 # Helper functions for managing the cart
-def __create_new_cart():
-    new_cart = Cart()
-    new_cart.save()
-    print("Created new cart with id " + str(new_cart.id))
-    return new_cart
 
-def __open_or_create_cart(request):
-     # Check if we have an open cart
-    try:
-        cart = request.session['cart_id']
-        print("Active cart " + request.session['cart_id'])
-        if Cart.objects.filter(pk=int(request.session['cart_id'])).exists():
-            cart = Cart.objects.get(pk=int(request.session['cart_id']))
-        else:
-            # This can happen if the cart was deleted during the session
-            cart = __create_new_cart()
-            request.session['cart_id'] = str(cart.id)
-        return cart
-    except KeyError:
-        print("No active cart")
-        cart = __create_new_cart()
-        request.session['cart_id'] = str(cart.id)
-        request.session.set_expiry(0)
-        return cart
-
-
-def __is_cart_open(request):
-    if not 'cart_id' in request.session.keys():
-        return False
-    if not Cart.objects.filter(pk=int(request.session['cart_id'])).exists():
-        return False
-    return True
 
 
 
@@ -46,8 +16,8 @@ def menu(request):
     """Display the list of offered pizzas"""
     pizzas = MenuPizza.objects.all()
     customer_cart = None
-    if __is_cart_open(request):
-        customer_cart = __open_or_create_cart(request)
+    if is_cart_open(request):
+        customer_cart = open_or_create_cart(request)
     context = { 'available_pizzas': pizzas, 'cart': customer_cart }
     return render(request, 'orders/menu.html', context)
 
@@ -77,29 +47,17 @@ def push_on_cart(request, pizza_id, size_id):
         else:
             print(key + " is not in toppings")
             collected_toppings = True
-    # replace toppings[i] with the referenced topping object
-    for i in range(len(toppings)):
-        toppings[i] = MenuTopping.objects.get(pk=int(toppings[i]))
 
     # Check if we have an open cart or create a new one
-    customer_cart = __open_or_create_cart(request)
+    customer_cart = open_or_create_cart(request)
 
     # Push the selected pizza and toppings onto the cart
-    pizza = OrderPizza(pizza=MenuPizza.objects.get(pk=pizza_id), cart=customer_cart)
-    pizza.save()
-    size = OrderSize(size=MenuSize.objects.get(pk=size_id), pizza=pizza)
-    size.save()
-    print("Ordered pizza: " + str(pizza))
-    print("Ordered size: " + str(size))
-    for topping in toppings:
-        top = OrderTopping(topping=topping, pizza=pizza)
-        top.save()
-        print("Ordered topping: " + str(top))
+    push_pizza_on_cart(customer_cart, pizza_id, size_id, toppings)
 
     return HttpResponseRedirect('/cart/show')
 
 def show_cart(request): 
-    customer_cart = __open_or_create_cart(request)
+    customer_cart = open_or_create_cart(request)
     if customer_cart.is_empty():
         context = { 'is_empty': True, 'pizzas': None }
         return render(request, 'orders/show_cart.html', context)
@@ -109,25 +67,24 @@ def show_cart(request):
         return render(request, 'orders/show_cart.html', context)
 
 def clear_cart(request):
-    if __is_cart_open(request):
+    if is_cart_open(request):
        del request.session['cart_id']
     return HttpResponseRedirect('/')
 
 def confirm_order(request):
     """Show a form asking for the clients name and address"""
     cart = None
-    if __is_cart_open(request):
-        cart = __open_or_create_cart(request)
+    if is_cart_open(request):
+        cart = open_or_create_cart(request)
     else:
         return HttpResponseRedirect('/') # Prevent empty orders
     return render(request, 'orders/confirm_order.html', {'cart': cart, 'total': cart.total_readable()})
 
 def place_order(request):
-    if not __is_cart_open(request):
+    if not is_cart_open(request):
         return HttpResponseRedirect('/') # Prevent empty orders
-    cart = __open_or_create_cart(request)
-    new_order = Order(from_cart=cart, customer_name=request.POST['name'], customer_address=request.POST['addr'])
-    new_order.save()
+    cart = open_or_create_cart(request)
+    new_order = create_order_from_cart(cart) 
     print("Created order " + str(new_order))
     return HttpResponseRedirect('/order/thanks')
 
